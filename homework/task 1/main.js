70889b214ed58341122bc4d3528caebb2976664a
kuchus <kuchus2010@gmail.com>
2024-12-28 19:56:27 +0200

    task1 versionfinal

--- homework/task 1/main.js
+++ homework/task 1/main.js
@@ -2,47 +2,35 @@ const numberDays = document.querySelector("input")
 const btn = document.querySelector("button")
 const answer = document.querySelector("h2")
 
+
+
+
 btn.onclick = function() {
+    const monday = numberDays.value == 1
+    const tuesdy = numberDays.value == 2
+    const wenday = numberDays.value == 3
+    const thursday = numberDays.value == 4
+    const friday = numberDays.value == 5
+    const saturday = numberDays.value == 6
+    const sunday = numberDays.value == 7
     const compareDays = numberDays.value <= 7 ;
-    console.log(compareDays)
-    if(compareDays) {
-        const monday = numberDays.value == 1
-        if(monday) {
-            answer.textContent = "Понедельник"
-        } else {
-            const tuesdy = numberDays.value == 2
-            if(tuesdy) {
-                answer.textContent = "Вторник"
-            } else {
-                const wenday = numberDays.value == 3
-                if(wenday) {
-                    answer.textContent = "Среда"
-                } else {
-                    const thursday = numberDays.value == 4
-                    if(thursday) {
-                        answer.textContent = "Четверг"
-                    } else {
-                        const friday = numberDays.value == 5
-                        if(friday) {
-                            answer.textContent = "Пятница"
-                        } else {
-                            const saturday = numberDays.value == 6
-                            if(saturday) {
-                                answer.textContent = "Субота"
-                            } else {
-                                const sunday = numberDays.value == 7
-                                if(sunday) {
-                                    answer.textContent = "Воскресенье"
-                                }
-                            }
-                        }            
-                    }
-                }
-                console.log(wenday)
-            }
-            
-        }
-    } else {
-        answer.textContent = "Ошибка:введите число от 1 до 7"
+   if(compareDays) {
+    if (monday) {
+        answer.textContent = "Понедельник"
+    } else if (tuesdy) {
+        answer.textContent = "Вторник"
+    } else if (wenday) {
+        answer.textContent = "Среда"     
+    } else if (thursday) {
+        answer.textContent = "Четверг"
+    } else if (friday) {
+        answer.textContent = "Пятница"
+    } else if (saturday) {
+        answer.textContent = "Субота"
+    } else if (sunday) {
+        answer.textContent = "Воскресенье"
     }
+   } else {
+       answer.textContent = "Ошибка:введите число от 1 до 7"
+   }
 }
